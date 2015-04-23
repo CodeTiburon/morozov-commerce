faf1060ad22c25362821423dc92a8f4622510ba3
Ivan <condenser0071@yandex.ua>
2015-04-23 18:00:10 +0300

    second encounter

---
+++ app/Http/Controllers/admin/AUserController.php
@@ -0,0 +1,92 @@
+<?php namespace App\Http\Controllers\admin;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+use Illuminate\Contracts\Auth\Registrar;
+use Illuminate\Http\Request;
+
+class AUserController extends Controller {
+
+    /**
+     * @param Registrar $registrar
+     */
+    public function __construct(Registrar $registrar)
+    {
+        $this->middleware('auth');
+    }
+
+	/**
+	 * Display a listing of the resource.
+	 *
+	 * @return Response
+	 */
+	public function index()
+	{
+        return view('admin/home', ['name' => 'James']);
+	}
+
+	/**
+	 * Show the form for creating a new resource.
+	 *
+	 * @return Response
+	 */
+	public function create()
+	{
+		//
+	}
+
+	/**
+	 * Store a newly created resource in storage.
+	 *
+	 * @return Response
+	 */
+	public function store()
+	{
+		//
+	}
+
+	/**
+	 * Display the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function show($id)
+	{
+		//
+	}
+
+	/**
+	 * Show the form for editing the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function edit($id)
+	{
+		//
+	}
+
+	/**
+	 * Update the specified resource in storage.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function update($id)
+	{
+		//
+	}
+
+	/**
+	 * Remove the specified resource from storage.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function destroy($id)
+	{
+		//
+	}
+
+}
